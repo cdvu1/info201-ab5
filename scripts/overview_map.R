04f94d7bf3c928c80412a534891a50753f9395a9
jody tran <jtranx@uw.edu>
2017-11-30 14:29:08 -0800

    created incomplete function

--- scripts/overview_map.R
+++ scripts/overview_map.R
@@ -3,9 +3,34 @@ library(httr)
 library(dplyr)
 source("api_key.R")
 
-base.uri <- 'https://api.data.gov/ed/collegescorecard/v1/schools/'
-query.params <- list(api_key = api.key, fields = "2015.student.share_firstgeneration")
-response <- GET(base.uri, query = query.params)
-content <- content(response, "text")
-body.data <- fromJSON(content) #extract and parse
-firstgen.data <- body.data$results
+#Jody's working directory
+setwd("~/Downloads/INFO201/info201-ab5")
+
+
+# the error that i get "No encoding supplied: defaulting to UTF-8."
+GetData <- function(input.state) {
+  base.uri <- 'https://api.data.gov/ed/collegescorecard/v1/schools/'
+  query.params <- list(api_key = api.key, fields = "2015.student.share_firstgeneration")
+  response <- GET(base.uri, query = query.params)
+  content <- content(response, "text")
+  body.data <- fromJSON(content) #extract and parse
+  firstgen.data <- data.frame()
+  
+  #get total number of pages by dividing total data and num of data per page
+  all.pages <- trunc(body.data$metadata$total / body.data$metadata$per_page)
+  
+  #for loop to each page and add that page's data into state.data
+  for(p in 1:all.pages) {
+    query.params$fields <- "2015.student.share_firstgeneration"
+    query.params$page <- p
+    response <- GET(base.uri, query = query.params)
+    content <- content(response, "text")
+    body.data <- fromJSON(content) #extract and parse
+    page.data <- flatten(body.data$results) 
+    firstgen.data <- rbind(firstgen.data, page.data) #merging the current state data with the current page data
+  }
+  return(firstgen.data)
+}
+
+GetData("Washington")
+
