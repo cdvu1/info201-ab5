fc8ef9010f01b9af9acd2c2917d3cbf1d6b14726
jody tran <jtranx@uw.edu>
2017-12-01 13:08:00 -0800

    okdja

--- scripts/overview_map.R
+++ scripts/overview_map.R
@@ -8,29 +8,35 @@ setwd("~/Downloads/INFO201/info201-ab5")
 
 
 # the error that i get "No encoding supplied: defaulting to UTF-8."
-GetData <- function(input.state) {
+GetData <- function(input.school) {
   base.uri <- 'https://api.data.gov/ed/collegescorecard/v1/schools/'
-  query.params <- list(api_key = api.key, fields = "2015.student.share_firstgeneration")
+  query.params <- list(api_key = jodys.key, fields = "school.state")
   response <- GET(base.uri, query = query.params)
   content <- content(response, "text")
   body.data <- fromJSON(content) #extract and parse
-  firstgen.data <- data.frame()
+  
+  str(body.data)
+  names(body.data)
+  items <- body.data$items
+  is.data.frame(items)
+  
+  state.data <- data.frame()
   
   #get total number of pages by dividing total data and num of data per page
   all.pages <- trunc(body.data$metadata$total / body.data$metadata$per_page)
   
   #for loop to each page and add that page's data into state.data
   for(p in 1:all.pages) {
-    query.params$fields <- "2015.student.share_firstgeneration"
+    query.params$fields <- "school.state"
     query.params$page <- p
     response <- GET(base.uri, query = query.params)
     content <- content(response, "text")
     body.data <- fromJSON(content) #extract and parse
     page.data <- flatten(body.data$results) 
-    firstgen.data <- rbind(firstgen.data, page.data) #merging the current state data with the current page data
+    state.data <- rbind(state.data, page.data) #merging the current state data with the current page data
   }
-  return(firstgen.data)
+  return(state.data)
 }
 
-GetData("Washington")
+GetData("OR")
 
